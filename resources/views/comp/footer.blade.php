5dad9289a92ffe4010e823cd9fa672f64d3518dd
Yasser <azisalfalahyasser@gmail.com>
2024-02-17 20:17:03 +0700

    chore : footer

---
+++ resources/views/comp/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="container">
+    <div class="text-center mt-3 p-2">
+        <p>Copyright & Created by Yasser <i class="bi bi-heart-fill" style="color: #D22727;"></i></p>
+    </div>
+    <div class="footer-right"></div>
+</footer>
